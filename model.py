3ec90e5b5327babb51826a5c430e1818c00dfb33
Jaka Basej <jaka.basej@student.fmf.uni-lj.si>
2019-05-21 15:54:44 +0200

    jae

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ PONOVLJENA_CRKA = 'o'
 NAPACNA_CRKA ='-'
 ZMAGA='W'
 PORAZ='X' 
+ZACETEK='Z'
 bazen_besed=[]
 a = 0
 
@@ -74,3 +75,28 @@ def nova_igra():
 
 
 
+class Vislice:
+    def __init__(self):
+        self.igre = {}
+
+    def prost_id_igre(self):
+        return len(self.igre)
+
+    def nova_igra(self):
+        id =self.prost_id_igre()
+        self.igre[id]=(nova_igra(),ZACETEK)
+        return id
+
+    def ugibaj(self,id_igre,crka):
+        igra, _ = self.igre[id_igre]
+        poskus = igra.ugibaj(crka)
+        self.igre[id_igre] = (igra,poskus)
+
+
+
+##################
+
+v= Vislice()
+v.nova_igra()
+v.nova_igra()
+print(v.igre)
